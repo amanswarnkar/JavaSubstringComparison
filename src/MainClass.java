fea264eee0aff8b950b1d0dacfb60bf823db7269
Aman Swarnkar <amanswarnkar60@gmail.com>
2019-01-19 23:56:01 +0530

    first commit

---
+++ src/MainClass.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int k = sc.nextInt();
+		sc.close();
+		
+		System.out.println(getSmallestAndLargest(s,k));
+	}
+
+	private static List<String> getSmallestAndLargest(String s, int k) {
+		String smallest = "";
+		String largest = "";
+		List<String> stringList = new ArrayList<>();
+		for(int i=0; i<(s.length()-k); i++) {
+			stringList.add(s.substring(i, i+k));
+		}
+		
+		return stringList;
+	}
+
+}
