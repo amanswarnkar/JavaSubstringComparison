76c53b07550378ccc55a857f157025d13ea59b7a
Aman Swarnkar <amanswarnkar60@gmail.com>
2019-01-20 01:15:03 +0530

    cli test

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class MainClass {
 
 	public static void main(String[] args) {
+		System.out.println("aman");
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
 		int k = sc.nextInt();
